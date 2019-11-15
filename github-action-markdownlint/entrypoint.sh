9688f7fb63a5dcd80bc83265f009c6b1acac52bc
jonasbn <jonasbn@dk-hostmaster.dk>
2019-11-15 10:37:26 +0100

    Updated entrypoint and attempt to address issue with output buffering for STDOUT

--- github-action-markdownlint/entrypoint.sh
+++ github-action-markdownlint/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh -l
 
-echo ""
-echo "Using Markdownlint on all Markdown files"
-echo "----------------------------------------"
+stdbuf -i0 -o0 -e0 echo "\n\nUsing Markdownlint on all Markdown files"
+stdbuf -i0 -o0 -e0 echo "----------------------------------------\n"
 
-find . -name "*.md" | xargs markdownlint
+find . -name "*.md" | xargs -t markdownlint
+
+EXITCODE=$?
+
+test $EXITCODE -eq 0 && echo "Everything looks good" || echo "Markdown does not comply with Markdownlint configuration";
+exit $EXITCODE
